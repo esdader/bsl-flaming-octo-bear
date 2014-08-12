d96513a9b3a9105bb305fc2ddde386385b988b9e
Tristan Ader <tristanader@gmail.com>
2014-08-12 16:47:32 -0500

    added google fonts

--- header.php
+++ header.php
@@ -9,7 +9,19 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
-    
+    <script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'PT+Sans:400,700,400italic,700italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>    
     <?php wp_head(); ?>
 </head>
 <body>
