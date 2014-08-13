9cd5eccf28b8a4d4aa86b31d5755ed14fbf3af20
Tristan Ader <tristanader@gmail.com>
2014-08-13 10:02:10 -0500

    quick push of frontpage

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php get_header(); ?>
 
-<div class="l-container">
-
+<div class="l-container-con">
+	<div class="l-container">
+		<div class="home-section">
+			<div class="home-copy">
+				<h1 class="col-10 offset-1 home-title">
+					We deliver behavior changing insights for our clients. 
+				</h1>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
