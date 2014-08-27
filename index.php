73344814e5d1e3c648d58a598218bf1bc8fb67c6
Tristan Ader <tristanader@gmail.com>
2014-08-27 16:08:05 -0500

    first pass frontpage video

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
 
 <div class="l-container-con">
-	<div class="l-container">
-		<div class="home-section">
-			<div class="home-copy">
-				<h1 class="col-10 offset-1 home-title">
-					We deliver behavior changing insights for our clients. 
-				</h1>
-			</div>
+	<div class="home-section">
+		<div class="video-container">
+			<video autoplay loop>
+					<source src="<?php echo bloginfo('template_directory'); ?>/videos/test_video2.mp4" type="video/mp4">
+			</video>
 		</div>
-	</div>
+		<div class="home-copy">
+			<h1 class="col-10 offset-1 home-title">
+				We deliver behavior changing insights for our clients. 
+			</h1>
+		</div>
 </div>
 
 <?php get_footer(); ?>
