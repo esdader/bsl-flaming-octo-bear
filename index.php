382074842b7d7465d59b4a3eb6dbed543fdcb14b
Tristan Ader <tristanader@gmail.com>
2014-09-26 10:36:12 -0500

    adding first pass at full width video

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php get_header(); ?>
 
-<div class="l-container-con">
+<div class="l-container">
 	<div class="home-section">
 		<div class="video-container">
 			<video autoplay loop>
 					<source src="<?php echo bloginfo('template_directory'); ?>/videos/test_video2.mp4" type="video/mp4">
 			</video>
 		</div>
-		<div class="home-copy">
+		<!-- <div class="home-copy">
 			<h1 class="col-10 offset-1 home-title">
 				We deliver behavior changing insights for our clients. 
 			</h1>
-		</div>
+		</div> -->
+	</div>
 </div>
 
 <?php get_footer(); ?>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,73 @@
+(function ($, window) {
+    'use strict';
 
+    var $header      = $('.l-header-con'),
+        $footer      = $('.l-short-footer-con'),
+        photoHeight   = 540,
+        photoWidth   = 960,
+        photoRatio   = photoWidth / photoHeight,
+        $window      = $(window),
+        $el          = $('video'),
+        $videoCon    = $('.video-container'),
+        availableHeight,
+        aspectRatio,
+        sideCrop,
+        imageWidth,
+        winH,
+        winW;
+
+    function findAvailableSpace() {
+        var headerH = $header.outerHeight(),
+            footerH = $footer.outerHeight();
+
+        winH = $window.height();
+        winW = $window.width();
+
+        availableHeight = winH - headerH - footerH + 5;
+        aspectRatio = winW / availableHeight;
+
+        $videoCon.css({
+            width: winW + 'px',
+            height: availableHeight + 'px',
+            overflow: 'hidden'
+        });
+
+        scaleLayout();
+    }
+
+    function scaleLayout() {
+        if (aspectRatio > photoRatio) {
+            var curHeight = photoRatio * availableHeight,
+                marginTop = (availableHeight - curHeight) / 2;
+            console.log(curHeight);
+            $el.css({
+                'margin-left' : '0',
+                'margin-top' : marginTop + 'px',
+                'width' : winW + 'px',
+                'height' : curHeight + 'px'
+            });
+
+        } else {
+            console.log('not big aspectRatio');
+            var marginLeft;
+
+            sideCrop = (winH *  photoRatio - winW) / 2;
+            imageWidth = winH * photoRatio;
+            marginLeft = '-' + sideCrop + 'px';
+            $el.css({
+                'margin-top' : '0',
+                'margin-left' : '-'+sideCrop + 'px',
+                'width' : imageWidth + 'px',
+                'height' : availableHeight + 'px'
+            });
+
+        }
+    }
+
+    $window.resize(findAvailableSpace);
+
+    findAvailableSpace();
+
+
+
+}) (jQuery, window);
